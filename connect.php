a43bc6b94a677eb3a4ac9cd36babf2427111a092
gungjagat <97139192+gungjagat@users.noreply.github.com>
2023-11-20 09:44:05 +0800

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+
+    $server     ="localhost";
+    $user       ="root";
+    $password   ="";
+    $database   ="2101020028_Anak Agung Ary Jagat Pranatha";
+
+    $con =mysqli_connect($server,$user,$password,$database);
+
+?>
